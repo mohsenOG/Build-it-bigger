a937422481af50ef69a5411a94b271730cfdc252
wonderfulme <mohsen_og@yahoo.com>
2018-05-24 15:26:16 +0200

    Adding android library and its dependencies.

---
+++ libandroid/src/main/java/com/wonderfulme/libandroid/JokeActivity.java
@@ -0,0 +1,26 @@
+package com.wonderfulme.libandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    public final String INTENT_JOKE = "INTENT_JOKE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        Intent receivedIntent = getIntent();
+        if (getIntent() == null) {
+            throw new RuntimeException(this.toString() + "Must include a joke");
+        }
+        String joke = receivedIntent.getStringExtra(INTENT_JOKE);
+
+        TextView jokeTextView = findViewById(R.id.tv_joker);
+        jokeTextView.setText(joke);
+    }
+}
